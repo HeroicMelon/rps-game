7a705b18a66b9adc7995e5ec4172d8a2ce9cf73c
Matthew <114954965+HeroicMelon@users.noreply.github.com>
2022-12-13 20:16:00 -0500

    playable rps with buttons

--- javascript.js
+++ javascript.js
@@ -1,62 +1,129 @@
 let playerScore = 0;
 let compScore = 0;
+let playerSelection;
+let computerSelection = getComputerChoice();
+
+const player = document.querySelector('#play-score');
+player.textContent = `Score: ${playerScore}`;
+
+const computer = document.querySelector('#comp-score');
+computer.textContent = `Score: ${compScore}`;
+
+const output = document.querySelector('#output');
+output.style.fontSize = '12px';
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = getComputerChoice();
+
+        if (playerSelection == 'rock') {
+            document.getElementById('playChoice').src ='images/rock.png';
+        } else if (playerSelection == 'paper') {
+            document.getElementById('playChoice').src ='images/paper.png';
+        } else if (playerSelection == 'scissors') {
+            document.getElementById('playChoice').src ='images/scissors.png';
+        }
+        if (computerSelection == 'rock') {
+            document.getElementById('compChoice').src ='images/rock.png';
+        } else if (computerSelection == 'paper') {
+            document.getElementById('compChoice').src ='images/paper.png';
+        } else if (computerSelection == 'scissors') {
+            document.getElementById('compChoice').src ='images/scissors.png';
+        }
+        game();
+    });
+});
+
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', () => {
+    resetGame();
+});
+
+const disableRock = document.getElementById('rock');
+const disablePaper = document.getElementById('paper');
+const disableScissors = document.getElementById('scissors');
+
+// game functions
 
 function getComputerChoice() { // randomizes computer's choice
     let choice = ['rock', 'paper', 'scissors'];
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
+
     if (playerSelection == 'rock' && computerSelection == 'paper') {
-        message = 'You lose! Paper beats Rock!';
+        output.textContent = 'You lose! Paper beats Rock!';
+        compScore++;
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        message = 'You win! Paper beats Rock!';
+        output.textContent = 'You win! Paper beats Rock!';
+        playerScore++;
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        message = 'You lose! Scissors beats Paper!';
+        output.textContent = 'You lose! Scissors beats Paper!';
+        compScore++;
     }
     else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        message = 'You win! Scissors beats Paper!';
+        output.textContent = 'You win! Scissors beats Paper!';
+        playerScore++;
     }
     else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        message = 'You lose! Rock beats Scissors!';
+        output.textContent = 'You lose! Rock beats Scissors!';
+        compScore++;
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    message = 'You win! Rock beats Scissors!';
+        output.textContent = 'You win! Rock beats Scissors!';
+        playerScore++;
     }
     else if (playerSelection == computerSelection) {
-        message = 'It\'s a tie!';
-    } else {
-        message = 'Invalid answer';
-    }
-    return message;
+        output.textContent = 'It\'s a tie!';
+    } 
 }
-
 
 function game() { // the game itself
-    let playerSelection = prompt("rock, paper, or scissors?").toLowerCase(); // changes player answer to lowercase
-    let computerSelection = getComputerChoice();
 
-    let roundResult = playRound(playerSelection, computerSelection);
-    if (roundResult.search('You win!') > -1) { // returns -1 if no match found
-        playerScore++;
-        console.log('You win! Player - ' + `${playerScore}` + ' Computer - ' + `${compScore}`);
-    } else if (roundResult.search('You lose!') > -1) {
-        compScore++;
-        console.log('You lose! Player - ' + `${playerScore}` + ' Computer - ' + `${compScore}`);
-    } else if (roundResult.search('tie!') > -1) {
-        console.log('It\'s a tie! Player - ' + `${playerScore}` + ' Computer - ' + `${compScore}`);
-    } else {
-        console.log('Invalid answer');
-    }
+    playRound(playerSelection, computerSelection);
+    player.textContent = `Score: ${playerScore}`;
+    computer.textContent = `Score: ${compScore}`;
+    
     endGame();
 }
-game();
-function endGame() { // ends the game when score reaches 5
+
+// helper functions
+
+function endGame() { // ends the game, disables buttons from continuing game, shows reset button
     if (playerScore >= 5 && compScore < 5) {
-        alert('Game over, You win!');
+        output.textContent = 'GG EZ, Play again?';
+        disableRock.disabled = true;
+        disablePaper.disabled = true;
+        disableScissors.disabled = true;
+        showResetBtn();
     } else if (playerScore < 5 && compScore >= 5) {
-        alert('Game over, You lose!');
+        output.textContent = 'Git gud scrub. Play again?'; 
+        disableRock.disabled = true;
+        disablePaper.disabled = true;
+        disableScissors.disabled = true;
+        showResetBtn();
     }
+}
+
+function showResetBtn() { // shows reset button
+    document.getElementById('reset').style.visibility = 'visible';
+}
+
+function resetGame() {
+    playerScore = 0;
+    compScore = 0;
+    player.textContent = `Score: ${playerScore}`;
+    computer.textContent = `Score: ${compScore}`;
+    output.textContent = 'GLHF';
+    disableRock.disabled = false;
+    disablePaper.disabled = false;
+    disableScissors.disabled = false;
+    resetBtn.style.visibility = 'hidden';
+    document.getElementById('playChoice').src ='images/question.png';
+    document.getElementById('compChoice').src ='images/question.png';
 }
